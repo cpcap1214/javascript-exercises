ea602fe5e9f8df2bd17117325a4977a86974abb9
cpcap1214 <hi1214justin@gmail.com>
2024-04-29 23:40:49 +0800

    Finish 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let ans = "";
+    if(times < 0) {
+        return "ERROR";
+    }
+    else {
+        for(let i = 0 ; i < times ; i++) {
+            ans += word;
+        }
+        return ans;
+    }
 };
 
 // Do not edit below this line
