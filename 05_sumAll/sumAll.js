7c5e11d5cb44bc8450677d6a896a23429ea6c981
cpcap1214 <hi1214justin@gmail.com>
2024-04-30 00:16:06 +0800

    Finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(para_one, para_two) {
+    if(para_one < 0 || para_two < 0) return "ERROR";
+    if(typeof para_one !== "number" || typeof para_two !== "number") return "ERROR";
+    if(para_one > para_two) {
+        let temp = para_one;
+        para_one = para_two;
+        para_two = temp;
+    }
+    let ans = 0;
+    for(let i = para_one ; i <= para_two ; i++){
+        ans += i;
+    }
+    return ans;
 };
 
 // Do not edit below this line
