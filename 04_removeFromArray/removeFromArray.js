a1a58583db1871b21a465321b09f5ab41f8e9564
cpcap1214 <hi1214justin@gmail.com>
2024-04-30 00:08:06 +0800

    Finish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...ele) {
+    let ans = [];
+    for(let i = 0 ; i < arr.length ; i++) {
+        if(!ele.includes(arr[i])) {
+            ans.push(arr[i]);
+        }
+    }
+    return ans;
 };
 
 // Do not edit below this line
